df03e6593f250c60f9c7e3fee84fd0f0ecbb688e
Tiago Ceccon <ceccon.t@hotmail.com>
2020-09-27 18:30:45 -0300

    Mix currencies

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -44,11 +44,11 @@ public class Money implements Expression {
                 '}';
     }
 
-    public Money times(int multiplier){
+    public Expression times(int multiplier){
         return new Money(amount * multiplier, currency);
     }
 
-    public Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 }
